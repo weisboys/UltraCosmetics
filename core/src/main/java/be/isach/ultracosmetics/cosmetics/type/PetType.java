a00930d6a3154f8b1677e4378cb19f13234e2af9
Chris <65610536+Chris6ix@users.noreply.github.com>
2022-09-24 19:01:21 +0200

    Disable Ghast

--- core/src/main/java/be/isach/ultracosmetics/cosmetics/type/PetType.java
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/type/PetType.java
@@ -132,7 +132,8 @@ public final class PetType extends CosmeticEntType<Pet> {
         new PetType("ZombifiedPiglin", XMaterial.GOLDEN_SWORD, EntityType.ZOMBIFIED_PIGLIN, PetZombifiedPiglin.class);
         new PetType("ElderGuardian", XMaterial.PRISMARINE_CRYSTALS, EntityType.ELDER_GUARDIAN, PetElderGuardian.class);
         new PetType("Endermite", XMaterial.ENDER_EYE, EntityType.ENDERMITE, PetEndermite.class);
-        new PetType("Ghast", XMaterial.FIRE_CHARGE, EntityType.GHAST, PetGhast.class);
+        /* Ghast disabled because its not moving at all (besides teleport) */
+        /* new PetType("Ghast", XMaterial.FIRE_CHARGE, EntityType.GHAST, PetGhast.class); */
         new PetType("Guardian", XMaterial.PRISMARINE_SHARD, EntityType.GUARDIAN, PetGuardian.class);
 
         // Cases fall through, so for example v1_19 gets all pets of 1.19 and below.
