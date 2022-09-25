5d0b53a429f429eea9740e4f5ce285aee205fb51
Chris <65610536+Chris6ix@users.noreply.github.com>
2022-09-25 23:20:35 +0200

    Create PetMagmaCube.java

---
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/pets/PetMagmaCube.java
@@ -0,0 +1,17 @@
+package be.isach.ultracosmetics.cosmetics.pets;
+
+import be.isach.ultracosmetics.UltraCosmetics;
+import be.isach.ultracosmetics.cosmetics.type.PetType;
+import be.isach.ultracosmetics.player.UltraPlayer;
+
+/**
+ * Represents an instance of a magma cube pet summoned by a player.
+ *
+ * @author Chris6ix
+ * @since 25-09-2022
+ */
+public class PetMagmaCube extends Pet {
+    public PetMagmaCube(UltraPlayer owner, PetType type, UltraCosmetics ultraCosmetics) {
+        super(owner, type, ultraCosmetics);
+    }
+}
