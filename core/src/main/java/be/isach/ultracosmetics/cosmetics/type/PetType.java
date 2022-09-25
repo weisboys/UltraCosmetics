5437beaa047522073c18d59fbb84b16510f3fa13
Chris <65610536+Chris6ix@users.noreply.github.com>
2022-09-25 23:45:37 +0200

    Add Ravager to PetType

--- core/src/main/java/be/isach/ultracosmetics/cosmetics/type/PetType.java
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/type/PetType.java
@@ -164,6 +164,7 @@ public final class PetType extends CosmeticEntType<Pet> {
             new PetType("Ocelot", XMaterial.COD, EntityType.OCELOT, PetOcelot.class);
             new PetType("WanderingTrader", XMaterial.WANDERING_TRADER_SPAWN_EGG, EntityType.WANDERING_TRADER, PetWanderingTrader.class);
             new PetType("Pillager", XMaterial.CROSSBOW, EntityType.PILLAGER, PetPillager.class);
+            new PetType("Ravager", XMaterial.RAVAGER_SPAWN_EGG, EntityType.RAVAGER, PetRavager.class);
         case v1_13:
             new PetType("Cod", XMaterial.COD_BUCKET, EntityType.COD, PetCod.class);
             new PetType("Pufferfish", XMaterial.PUFFERFISH, EntityType.PUFFERFISH, PetPufferfish.class);
