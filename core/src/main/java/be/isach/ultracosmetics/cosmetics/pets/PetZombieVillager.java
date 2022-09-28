95257a9c8bfe2024aecab9c5e3e7b07a96d6fc43
Chris <65610536+Chris6ix@users.noreply.github.com>
2022-09-28 23:42:52 +0200

    Create PetZombieVillager.java

---
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/pets/PetZombieVillager.java
@@ -0,0 +1,17 @@
+package be.isach.ultracosmetics.cosmetics.pets;
+
+import be.isach.ultracosmetics.UltraCosmetics;
+import be.isach.ultracosmetics.cosmetics.type.PetType;
+import be.isach.ultracosmetics.player.UltraPlayer;
+
+/**
+ * Represents an instance of a zombie villager pet summoned by a player.
+ *
+ * @author Chris6ix
+ * @since 28-09-2022
+ */
+public class PetZombieVillager extends Pet {
+    public PetZombieVillager(UltraPlayer owner, PetType type, UltraCosmetics ultraCosmetics) {
+        super(owner, type, ultraCosmetics);
+    }
+}
