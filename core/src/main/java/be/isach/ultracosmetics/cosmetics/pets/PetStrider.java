96b3c9f6260e8b9a919e6aff4419eb53d35ccdf7
datatags <datatags@users.noreply.github.com>
2022-10-12 10:51:15 -0700

    Revert shulker customizations; add to messages

--- core/src/main/java/be/isach/ultracosmetics/cosmetics/pets/PetStrider.java
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/pets/PetStrider.java
@@ -4,8 +4,6 @@ import be.isach.ultracosmetics.UltraCosmetics;
 import be.isach.ultracosmetics.cosmetics.type.PetType;
 import be.isach.ultracosmetics.player.UltraPlayer;
 
-import org.bukkit.entity.Strider;
-
 /**
  * Represents an instance of a strider pet summoned by a player.
  *
@@ -13,20 +11,7 @@ import org.bukkit.entity.Strider;
  * @since 07-09-2022
  */
 public class PetStrider extends Pet {
-    private boolean shivering = false;
-
     public PetStrider(UltraPlayer owner, PetType type, UltraCosmetics ultraCosmetics) {
         super(owner, type, ultraCosmetics);
-    }
-
-    @Override
-    public void onUpdate() {
-        ((Strider) entity).setShivering(shivering);
-    }
-
-    @Override
-    public boolean customize(String customization) {
-        shivering = customization.equalsIgnoreCase("true");
-        return true;
     }
 }
