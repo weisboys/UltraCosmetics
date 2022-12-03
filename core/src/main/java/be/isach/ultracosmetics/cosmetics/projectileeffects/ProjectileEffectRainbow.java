d6f4cfef993d066cb46eceb959f5e78c085f1283
Dennis Herzberg <33426785+dennishzg@users.noreply.github.com>
2022-12-03 23:58:38 +0100

    impr(projectileeffects): improve Rainbow colors

--- core/src/main/java/be/isach/ultracosmetics/cosmetics/projectileeffects/ProjectileEffectRainbow.java
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/projectileeffects/ProjectileEffectRainbow.java
@@ -20,25 +20,34 @@ public class ProjectileEffectRainbow extends ProjectileEffect {
 
         switch (i) {
             case 1:
-                Particles.REDSTONE.display(222, 0, 0, projectile.getLocation());
+                Particles.REDSTONE.display(255, 0, 0, projectile.getLocation());
                 break;
             case 2:
-                Particles.REDSTONE.display(254, 98, 44, projectile.getLocation());
+                Particles.REDSTONE.display(255, 135, 0, projectile.getLocation());
                 break;
             case 3:
-                Particles.REDSTONE.display(254, 246, 0, projectile.getLocation());
+                Particles.REDSTONE.display(255, 211, 0, projectile.getLocation());
                 break;
             case 4:
-                Particles.REDSTONE.display(0, 188, 0, projectile.getLocation());
+                Particles.REDSTONE.display(222, 255, 10, projectile.getLocation());
                 break;
             case 5:
-                Particles.REDSTONE.display(0, 156, 254, projectile.getLocation());
+                Particles.REDSTONE.display(161, 255, 10, projectile.getLocation());
                 break;
             case 6:
-                Particles.REDSTONE.display(0, 0, 132, projectile.getLocation());
+                Particles.REDSTONE.display(10, 255, 153, projectile.getLocation());
                 break;
             case 7:
-                Particles.REDSTONE.display(44, 0, 156, projectile.getLocation());
+                Particles.REDSTONE.display(10, 239, 255, projectile.getLocation());
+                break;
+            case 8:
+                Particles.REDSTONE.display(20, 125, 245, projectile.getLocation());
+                break;
+            case 9:
+                Particles.REDSTONE.display(88, 10, 255, projectile.getLocation());
+                break;
+            case 10:
+                Particles.REDSTONE.display(190, 10, 255, projectile.getLocation());
                 i = 0;
                 break;
         }
