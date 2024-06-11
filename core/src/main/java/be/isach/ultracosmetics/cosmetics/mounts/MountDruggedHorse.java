d0aaf20bb92180304cb81a5373ca7207e55021ba
datatags <datatags@users.noreply.github.com>
2024-06-10 17:45:19 -0700

    Update XSeries to release and stop shading it

--- core/src/main/java/be/isach/ultracosmetics/cosmetics/mounts/MountDruggedHorse.java
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/mounts/MountDruggedHorse.java
@@ -20,6 +20,20 @@ import java.util.concurrent.ThreadLocalRandom;
  * Created by sacha on 10/08/15.
  */
 public class MountDruggedHorse extends MountAbstractHorse {
+    private static final Particle ENTITY_EFFECT_PARTICLE = XParticle.ENTITY_EFFECT.get();
+    private static final boolean SUPPORTS_ALPHA;
+    private static final boolean SUPPORTS_COLOR_DATA;
+
+    static {
+        Color color = null;
+        try {
+            color = Color.fromARGB(0);
+        } catch (NoSuchMethodError ignored) {
+            // Handled below
+        }
+        SUPPORTS_ALPHA = color != null;
+        SUPPORTS_COLOR_DATA = XParticle.ENTITY_EFFECT.get().getDataType() == Color.class;
+    }
 
     private Player effectPlayer;
 
@@ -43,15 +57,28 @@ public class MountDruggedHorse extends MountAbstractHorse {
     @Override
     public void onUpdate() {
         Location loc = entity.getLocation().add(0, 1, 0);
-        Particles.FIREWORK.display(0.4f, 0.2f, 0.4f, loc, 5);
-        Particles.EFFECT.display(0.4f, 0.2f, 0.4f, loc, 5);
-        Particle entityEffectParticle = XParticle.ENTITY_EFFECT.get();
-        ThreadLocalRandom r = ThreadLocalRandom.current();
+        //Particles.FIREWORK.display(0.4f, 0.2f, 0.4f, loc, 5);
+        //Particles.EFFECT.display(0.4f, 0.2f, 0.4f, loc, 5);
         for (int i = 0; i < 5; i++) {
-            Color color = Color.fromARGB(Particles.AMBIENT_ALPHA, r.nextInt(256), r.nextInt(256), r.nextInt(256));
-            entity.getWorld().spawnParticle(entityEffectParticle, loc, 1, 0.4f, 0.2f, 0.4f, color);
+            //coloredParticle(loc, null);
+            coloredParticle(loc, Color.fromRGB(5, 255, 0));
         }
-        entity.getWorld().spawnParticle(entityEffectParticle, loc, 5, 0.4f, 0.2f, 0.4f, Color.fromRGB(5, 255, 0));
+    }
+
+    private void coloredParticle(Location loc, Color color) {
+        if (color == null) {
+            ThreadLocalRandom r = ThreadLocalRandom.current();
+            if (SUPPORTS_ALPHA) {
+                color = Color.fromARGB(Particles.AMBIENT_ALPHA, r.nextInt(256), r.nextInt(256), r.nextInt(256));
+            } else {
+                color = Color.fromRGB(r.nextInt(256), r.nextInt(256), r.nextInt(256));
+            }
+        }
+        if (SUPPORTS_COLOR_DATA) {
+            entity.getWorld().spawnParticle(ENTITY_EFFECT_PARTICLE, loc, 0, 0.4, 0.2, 0.4, color);
+        } else {
+            entity.getWorld().spawnParticle(ENTITY_EFFECT_PARTICLE, loc, 0, color.getRed() / 255f, color.getGreen() / 255f, color.getBlue() / 255f);
+        }
     }
 
     @Override
