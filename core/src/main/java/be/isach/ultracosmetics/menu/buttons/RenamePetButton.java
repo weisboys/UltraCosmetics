de2a54fb4d47be0a173136608ea29c5c8b2e4bdb
datatags <datatags@users.noreply.github.com>
2024-07-03 10:00:10 -0700

    Try to handle outdated AnvilGUI

--- core/src/main/java/be/isach/ultracosmetics/menu/buttons/RenamePetButton.java
+++ core/src/main/java/be/isach/ultracosmetics/menu/buttons/RenamePetButton.java
@@ -77,7 +77,7 @@ public class RenamePetButton implements Button {
                             return Collections.singletonList(AnvilGUI.ResponseAction.close());
                         }
                     }).open(ultraPlayer.getBukkitPlayer());
-        } catch (ExceptionInInitializerError e) {
+        } catch (NoClassDefFoundError e) {
             ultraPlayer.sendMessage(MessageManager.getMessage("Use-Rename-Pet-Command"));
         }
     }
