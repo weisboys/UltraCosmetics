c08dbd7b4981bf90ce1e3e3458ae284a92c436e9
BySwiizen <byswiizen@gmail.com>
2024-11-02 17:43:10 +0100

    Update to 1.21.3

--- core/src/main/java/be/isach/ultracosmetics/version/ServerVersion.java
+++ core/src/main/java/be/isach/ultracosmetics/version/ServerVersion.java
@@ -14,7 +14,7 @@ public enum ServerVersion {
     v1_18(18, 2),
     v1_19(19, 4),
     v1_20(20, 6, "ee13f98a43b9c5abffdcc0bb24154460", 4),
-    v1_21(21, 2, "ec8b033a89c54252f1dfcb809eab710a", 2),
+    v1_21(21, 3, "INSERT MAPPING", 3),
     NEW("???"),
     ;
 
